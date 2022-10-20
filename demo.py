629b8dbfdcabb70f93013f4dfc548112233e0829
Junyu Zhao <93512090+theluckyzhao@users.noreply.github.com>
2022-10-20 16:06:26 +0900

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,41 @@
+# -- coding: utf-8 --
+import json
+
+path_all = (r"F:\Work\Ernie\sounds_Align\sounds_file\00005_04_observer_kojima_meeting_1.json")
+path_yama = (r"F:\Work\Ernie\sounds_Align\sounds_file\00005_01_onoyama_meeting.json")     # Open local files
+
+yama = open(path_yama, 'r', encoding='utf-8-sig')    # read json file, voice of Onoyama
+data_yama = json.load(yama, strict=False)    # return data in dict
+print(type(data_yama))
+
+All = open(path_all,encoding='utf-8-sig', errors='ignore')
+data_wata_1 = json.load(All, strict=False)
+
+for res in data_yama['response']['results']:
+
+     for alt in res['alternatives']:
+          print('-----------------------------------------------------------------------------')
+          print('transcript:', alt['transcript'])
+          print('words[0]:', alt['words'][0]['word'], alt['words'][0]['startTime'])
+          print('words[-1]:', alt['words'][-1]['word'],  alt['words'][-1]['endTime'])
+          print('')
+             
+          for res_1 in data_wata_1['response']['results']:
+
+                    for alt_1 in res_1['alternatives']:
+                         # print('transcript_1:', alt_1['transcript'])
+                         # print('words[0]:', alt_1['words'][0]['word'], alt_1['words'][0]['startTime'])
+                         # print('words[-1]:', alt_1['words'][-1]['word'],  alt_1['words'][-1]['endTime'])
+                         # print('')
+                         # print('Onoyama: ',alt['transcript'])
+          
+                         if alt_1['transcript'] == alt['transcript']:
+                              alt['time_delay_start'] = alt_1['words'][0]['startTime'] - alt['words'][0]['startTime']
+                              print('time_delay_start is: ', alt['time_delay_start'])
+                              alt['time_delay_end'] = alt_1['words'][-1]['endTime'] - alt['words'][-1]['endTime']
+                              print('time_delay_end is: ', alt['time_delay_end'])
+                             
+                              path_yama_new = open(path_yama + '_new.json', 'a')
+                              json.dump(alt, path_yama_new)
+
+print('Onoyama finished.')
