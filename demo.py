4a88e0ce73b7d8e761449040e67a38468b22e6e9
Junyu Zhao <93512090+theluckyzhao@users.noreply.github.com>
2022-10-21 11:35:38 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,12 +4,12 @@ import json
 path_all = (r"F:\Work\Ernie\sounds_Align\sounds_file\00005_04_observer_kojima_meeting_1.json")
 path_yama = (r"F:\Work\Ernie\sounds_Align\sounds_file\00005_01_onoyama_meeting.json")     # Open local files
 
-yama = open(path_yama, 'r', encoding='utf-8-sig')    # read json file, voice of Onoyama
-data_yama = json.load(yama, strict=False)    # return data in dict
+yama = open(path_yama, 'rb')    # read json file, voice of Onoyama , encoding='utf-8-sig'
+data_yama = json.load(yama, strict=False)    # return data in dict, strict check close
 print(type(data_yama))
 
-All = open(path_all,encoding='utf-8-sig', errors='ignore')
-data_wata_1 = json.load(All, strict=False)
+All = open(path_all,'rb')     #  , encoding='utf-8-sig'  , errors='ignore'
+data_wata_1 = json.load(All)  #, strict=False
 
 for res in data_yama['response']['results']:
 
@@ -34,8 +34,9 @@ for res in data_yama['response']['results']:
                               print('time_delay_start is: ', alt['time_delay_start'])
                               alt['time_delay_end'] = alt_1['words'][-1]['endTime'] - alt['words'][-1]['endTime']
                               print('time_delay_end is: ', alt['time_delay_end'])
-                             
-                              path_yama_new = open(path_yama + '_new.json', 'a')
-                              json.dump(alt, path_yama_new)
+                              print(type(alt['transcript']))
+                                                          
+path_yama_new = open(path_yama + '_new.json', 'w', encoding="utf-8")  # Open new file in unicode, encoding='utf-8-sig'
+json.dump(res, path_yama_new, ensure_ascii=False)     #  ensure_ascii=False
 
 print('Onoyama finished.')
