e515b5820a7a74e349ba2128f9c94729e9f749f0
Junyu Zhao <93512090+theluckyzhao@users.noreply.github.com>
2022-11-10 17:46:27 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 # -- coding: utf-8 --
 
-__updated__ = '2022-11-08 14:41:51'
+__updated__ = '2022-11-10 17:46:01'
 
 import json
 import editdistance
@@ -14,23 +14,21 @@ print(type(data_yama))
 All = open(path_all,'rb')     #  , encoding='utf-8-sig'  , errors='ignore'
 data_wata_1 = json.load(All)  #, strict=False
 
-for res in data_yama['response']['results']:
-
-     for alt in res['alternatives']:
+######################_New_Meta!_############################
+for res_1 in data_wata_1['response']['results']:  # Open Oberver dictionary by loop
+     for alt_1 in res_1['alternatives']:     # Open alternatives, key before transcript
           print('-----------------------------------------------------------------------------')
-          print('transcript:', alt['transcript'])
-          print('words[0]:', alt['words'][0]['word'], alt['words'][0]['startTime'])
-          print('words[-1]:', alt['words'][-1]['word'],  alt['words'][-1]['endTime'])
-          print('')
-             
-          for res_1 in data_wata_1['response']['results']:
+          print('transcript:', alt_1['transcript'])
+          print('words[0]:', alt_1['words'][0]['word'], alt_1['words'][0]['startTime'])
+          print('words[-1]:', alt_1['words'][-1]['word'],  alt_1['words'][-1]['endTime'])
+          print('')         
+
+          for res in data_yama['response']['results']: # Open Onoyama dictionary by loop
                time_delay_start_list = list() # Create a new list for calculating minimum time error, start time
                time_delay_end_list = list() # For end time               
                transcript_split_value = list()
                transcript_split_word = list()
-
-               for alt_1 in res_1['alternatives']:
-                    
+               for alt in res['alternatives']:    # Open alternatives, key before transcript for Onoyama             
                     def simirallity (trans1, trans2):
                          simi = editdistance.eval(trans1, trans2)
                          return simi   
@@ -40,26 +38,28 @@ for res in data_yama['response']['results']:
                               tolerance = 1
                     else :
                          tolerance = 10
+                    time_tole = 50
+                    # time_diff = alt['words'][0]['startTime'] - alt_1['words'][0]['startTime']     # reduce error by using time difference     
+                    # if time_diff < time_tole:
 
-                    if simirallity (alt_1['transcript'], alt['transcript']) < tolerance:
-                                                  
+                    if simirallity (alt_1['transcript'], alt['transcript']) < tolerance:                                                  
                          TDS = alt_1['words'][0]['startTime'] - alt['words'][0]['startTime']
                          time_delay_start_list.append(TDS) # Recording in list of start time
-                         alt['time_delay_start'] = alt_1['words'][0]['startTime'] - alt['words'][0]['startTime']
-                         # print('time_delay_start is: ', alt['time_delay_start'])
+                         alt_1['time_delay_start'] = alt_1['words'][0]['startTime'] - alt['words'][0]['startTime']
+                         print('time_delay_start is: ', alt_1['time_delay_start'])
                          TDE = alt_1['words'][-1]['endTime'] - alt['words'][-1]['endTime']
                          time_delay_end_list.append(TDE) # Recording in list of end time
-                         alt['time_delay_end'] = alt_1['words'][-1]['endTime'] - alt['words'][-1]['endTime']
-                         # print('time_delay_end is: ', alt['time_delay_end'])
+                         alt_1['time_delay_end'] = alt_1['words'][-1]['endTime'] - alt['words'][-1]['endTime']
+                         print('time_delay_end is: ', alt_1['time_delay_end'])
 
-                         ##################################################
+                         ######################_Error_adjustment_############################
                          minabs_s = abs(time_delay_start_list[0])  # Find the minimum time delay of start time
                          minele_s = time_delay_start_list[0]  # and return in original value
                          for l in time_delay_start_list:
                               if abs(l) < minabs_s:
                                    minabs_s = abs(l)
                                    minele_s = l
-                         alt['time_delay_start'] = minele_s
+                         alt_1['time_delay_start'] = minele_s
                          # print('minimum start time delay is:', alt['time_delay_start'])   
                                             
                          minabs_e = abs(time_delay_end_list[0])  # Find the minimum time delay of end time
@@ -68,11 +68,10 @@ for res in data_yama['response']['results']:
                               if abs(l) < minabs_e:
                                    minabs_e = abs(l)
                                    minele_e = l
-                         ##################################################
-
-                         alt['time_delay_end'] = minele_e
+                         alt_1['time_delay_end'] = minele_e
                          # print('minimum end time delay is:', alt['time_delay_end'])    
-                    
+                         ######################_Error_adjustment_############################                    
+
                     i = 0
                     similarity_list = []
 
@@ -80,16 +79,16 @@ for res in data_yama['response']['results']:
                         similarity_list.append(editdistance.eval(alt_1['transcript'][i:len(alt['transcript'])+i], alt['transcript']))
                         i = i + 1
                     position = similarity_list.index(min(similarity_list)) # Position of minimum similarity 
-                    print('Split transcript in list No.', position, ', content ', alt_1['transcript'][position:position + len(alt['transcript'])]) 
-                    print('similarity_list is: ', similarity_list)
+                    # print('Split transcript in list No.', position, ', content ', alt_1['transcript'][position:position + len(alt['transcript'])]) 
+                    # print('similarity_list is: ', similarity_list)
 
                     transcript_split_value.append(min(similarity_list))    # Value of minimum similarity 
                     transcript_split_word.append(alt_1['transcript'][position:position + len(alt['transcript'])])  # Content of minimum similarity
                     mini_value = transcript_split_value.index(min(transcript_split_value))
-                    alt['transcript_split'] = transcript_split_word[mini_value] # Recording of content in dictionary, json file
-                    print('transcript_split_value: ', transcript_split_value)
+                    alt_1['transcript_split'] = transcript_split_word[mini_value] # Recording of content in dictionary, json file
+                    # print('transcript_split_value: ', transcript_split_value)
 
-                    print('End word of split transcript is', alt_1['words'][position:position + len(alt['transcript'])]) 
+                    # print('End word of split transcript is', alt_1['words'][position:position + len(alt['transcript'])]) 
 
                     # TDS = alt_1['words'][0]['startTime'] - alt['words'][0]['startTime']
                     # time_delay_start_list.append(TDS) # Recording in list of start time
@@ -97,10 +96,10 @@ for res in data_yama['response']['results']:
                     # TDE = alt_1['words'][-1]['endTime'] - alt['words'][-1]['endTime']
                     # time_delay_end_list.append(TDE) # Recording in list of end time
                     # alt['time_delay_end'] = alt_1['words'][-1]['endTime'] - alt['words'][-1]['endTime']
-            
-path_yama_new = open(path_yama + '_new.json', 'w', encoding="utf-8")  # Open new file in unicode, encoding='utf-8-sig'
-json.dump(data_yama, path_yama_new, ensure_ascii=False)     #  ensure_ascii=False
 
+path_all = path_all.split('.')[0] 
+path_all_new = open(path_all + '_new.json', 'w', encoding="utf-8")  # Open new file in unicode, encoding='utf-8-sig'
+json.dump(data_wata_1, path_all_new, ensure_ascii=False)     #  ensure_ascii=False
 print('\n Onoyama finished.')
 
 __updated__ = '2022-11-08 14:16:51'
