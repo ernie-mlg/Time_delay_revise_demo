569f117c769a44aabc2c7c75c492d2956a446d0f
Junyu Zhao <93512090+theluckyzhao@users.noreply.github.com>
2022-11-18 16:13:45 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,104 +1,126 @@
-# -- coding: utf-8 --
-
-__updated__ = '2022-11-14 15:21:42'
+__updated__ = '2022-11-18 16:12:36'
 
 import json
 import editdistance
+import glob
+from collections import Counter
 
-path_all = (r"F:\Work\Ernie\sounds_Align\sounds_file\00005_04_observer_kojima_meeting_1.json")      # Voice of all people
-path_yama = (r"F:\Work\Ernie\sounds_Align\sounds_file\00005_01_onoyama_meeting.json")     # Open local files of one person
-
-yama = open(path_yama, 'rb')    # read json file, voice of Onoyama , encoding='utf-8-sig'
-data_yama = json.load(yama, strict=False)    # return data in dict, strict check close
-print(type(data_yama))
-All = open(path_all,'rb')     #  , encoding='utf-8-sig'  , errors='ignore'
-data_wata_1 = json.load(All)  #, strict=False
-def simirallity (trans1, trans2):
-     simi = editdistance.eval(trans1, trans2)/len(trans2)
-     return simi
-j = 0
-
-######################_New_Meta!_############################
-for res_1 in data_wata_1['response']['results']:  # Open Oberver dictionary by loop
-     for alt_1 in res_1['alternatives']:     # Open alternatives, key before transcript
-          print('-----------------------------------------------------------------------------')
-          print('transcript of all:', alt_1['transcript'])
-          # print('words[0]:', alt_1['words'][0]['word'], 'startTime:', alt_1['words'][0]['startTime'])
-          # print('words[-1]:', alt_1['words'][-1]['word'],  'endTime:', alt_1['words'][-1]['endTime'])
-          print('')
-          alt_1['transcript_list'] = list()         
-          j += 1
-          
-          for res in data_yama['response']['results']: # Open Onoyama dictionary by loop
-               time_delay_start_list = list() # Create a new list for calculating minimum time error, start time
-               time_delay_end_list = list() # For end time               
-               transcript_split_value = list()
-               transcript_split_word = list()               
-               for alt in res['alternatives']:    # Open alternatives, key before transcript for Onoyama             
-   
-                    time_tole = 40 # Reduce time error in xx second. Please do not over than longest duration
-                    time_diff = abs(alt['words'][0]['startTime'] - alt_1['words'][0]['startTime'])     # reduce error by using time difference     
-                    if time_diff <= time_tole:
-                         i = 0
-                         similarity_list = []
-                         while (i <= abs(len(alt_1['transcript'])-len(alt['transcript']))):    # Similarity testing
-                             similarity_list.append(simirallity(alt_1['transcript'][i:len(alt['transcript'])+i], alt['transcript']))  # Recording similarity for every word in list
-                             i = i + 1
-                         position = similarity_list.index(min(similarity_list)) # Position of minimum similarity 
-                         print('Split transcript in list No.', position, ', content ', alt_1['transcript'][position:position + len(alt['transcript'])]) 
-                         print('pair transcript of onoyama is ', alt['transcript'])
-                         print('similarity_list is: ', similarity_list)
-
-                         transcript_split_value.append(min(similarity_list))    # Value of minimum similarity 
-                         transcript_split_word.append(alt_1['transcript'][position:position + len(alt['transcript'])])  # Content of minimum similarity
-                         mini_value = transcript_split_value.index(min(transcript_split_value))
-
-                         # if len(alt['transcript']) <= 9:
-                         #      tolerance = len(alt['transcript']) - 3
-                         #      if tolerance <= 0:
-                         #           tolerance = 2
-                         # else :
-                         #      tolerance = 10
-                         tolerance = 0.34    # difference permission smaller than 1/3 
-                         if simirallity (alt_1['transcript'][position:position + len(alt['transcript'])], alt['transcript']) < tolerance:                                                  
-                              TDS = alt_1['words'][0]['startTime'] - alt['words'][0]['startTime']
-                              time_delay_start_list.append(TDS) # Recording in list of start time
-                              alt_1['time_delay_start'] = alt_1['words'][0]['startTime'] - alt['words'][0]['startTime']
-                              # print('time_delay_start is: ', alt_1['time_delay_start'])
-                              TDE = alt_1['words'][-1]['endTime'] - alt['words'][-1]['endTime']
-                              time_delay_end_list.append(TDE) # Recording in list of end time
-                              alt_1['time_delay_end'] = alt_1['words'][-1]['endTime'] - alt['words'][-1]['endTime']
-                              # print('time_delay_end is: ', alt_1['time_delay_end'])
-
-                              ######################_Error_adjustment_############################
-                              minabs_s = abs(time_delay_start_list[0])  # Find the minimum time delay of start time
-                              minele_s = time_delay_start_list[0]  # and return in original value
-                              for l in time_delay_start_list:
-                                   if abs(l) < minabs_s:
-                                        minabs_s = abs(l)
-                                        minele_s = l
-                              alt_1['time_delay_start'] = minele_s
-                              # print('minimum start time delay is:', alt['time_delay_start'])   
-                                            
-                              minabs_e = abs(time_delay_end_list[0])  # Find the minimum time delay of end time
-                              minele_e = time_delay_end_list[0]  # and return in original value
-                              for l in time_delay_end_list:
-                                   if abs(l) < minabs_e:
-                                        minabs_e = abs(l)
-                                        minele_e = l
-                              alt_1['time_delay_end'] = minele_e
-                              # print('minimum end time delay is:', alt['time_delay_end'])    
-                              ######################_Error_adjustment_############################
-                                                  
-                              alt_1['transcript_split'] = alt['transcript'] # Recording of compare content from Onoyama
-                              # Need a loop for Onoyama transcript selecting                         
-                              alt_1['transcript_list'].append(transcript_split_word[mini_value])    # Recording all similar transcript from Onoyama to All in a list
-                              # print('transcript_split_value: ', transcript_split_value)
-                              # print('End word of split transcript is', alt_1['words'][position:position + len(alt['transcript'])]) 
+def similarity(str1, str2):
+    simi = editdistance.eval(str1, str2) / len(str2)
+    return 1 - simi
 
 
-path_all = path_all.split('.')[0] 
-path_all_new = open(path_all + '_new.json', 'w', encoding="utf-8")  # Open new file in unicode, encoding='utf-8-sig'
-json.dump(data_wata_1, path_all_new, ensure_ascii=False)     #  ensure_ascii=False
-print('\n Onoyama finished.')
+def find_suit_position(trans_obs, trans_spk):
+    similarity_list = []
+    i = 0
+    while (i <= abs(len(trans_obs) - len(trans_spk))):
+        similarity_list.append(similarity(trans_obs[i:len(trans_spk) + 1], trans_spk))
+        i += 1
+
+    pos_idx = similarity_list.index(max(similarity_list))
+    return pos_idx, similarity_list
+
+
+def get_time_delay(alt_obs, alt_spk):
+    TDS = alt_obs['words'][0]['startTime'] - alt_spk['words'][0]['startTime']
+    TDE = alt_obs['words'][-1]['endTime'] - alt_spk['words'][-1]['endTime']
+    return TDS, TDE
+
+def get_min_time_delay(time_delay_list):
+    min_abs = abs(time_delay_list[0])
+    min_ele = time_delay_list[0]
+
+    for l in time_delay_list:
+        if abs(l) < min_abs:
+            min_abs = abs(l)
+            min_ele = l
+    
+    return min_ele     
+
+# def get_trans_time(alt_obs, alt_spk):
+    
+    list_index = 0  # index in transcript_list
+    last_index = 0
+
+    for trans_list in alt_obs['transcript_list']: # loop of "transcript_list"
+        # for index in range(len(alt_obs['transcript_list'])-1):
+        words_num = 0   # index in list 'words', 'はい' = 0
+        len_word = 0    # length of word composition, 'はい' + '。' = 3
+        while list_index < len(alt_obs['transcript_list']): # 
+            while words_num <= len(alt_obs['words']):
+                if len_word < len(trans_list):
+                    len_word += len(alt_obs['words'][words_num]['word'])
+                else:
+                    TDS_split = alt_obs['words'][last_index]['startTime'] - alt_spk['words'][last_index]['startTime']
+                    TDE_split = alt_obs['words'][words_num-1]['endTime'] - alt_spk['words'][words_num-1]['endTime']
+                    list_index += 1
+                    last_index = words_num
+                words_num += 1
+        
+    return TDS_split, TDE_split
+
+def main():
+
+    path_obs_list = glob.glob(r"F:\Work\Ernie\sounds_Align\sounds_file\*observer*[!new]meeting_1.json")  # Voice of all people
+    path_spk_list = glob.glob(r"F:\Work\Ernie\sounds_Align\sounds_file\*[!observer]*meeting.json")     # Open local files of one person in list
+    for path_obs in path_obs_list:
+        data_obs = json.load(open(path_obs, 'rb'), strict=False)
+        for path_spk in path_spk_list:
+            data_spk = json.load(open(path_spk, 'rb'), strict=False)
+        
+            for res_obs in data_obs['response']['results']:
+                for alt_obs in res_obs['alternatives']:
+                
+                    print('---------------------------------')
+                    print('Observer:', alt_obs['transcript'])
+                    alt_obs['transcript_list'] = []
+                   
+                
+                    for res_spk in data_spk['response']['results']:
+                        time_delay_start_list = []
+                        time_delay_end_list = []
+                        transcript_split_value = []
+                        transcript_split_word = []
+                        for alt_spk in res_spk['alternatives']:
+                            time_diff = abs(alt_spk['words'][0]['startTime'] - alt_obs['words'][0]['startTime'])
+                            # if diff of time is less than 40 sec
+                            if time_diff <= 40:
+                                pos_idx, similarity_list = find_suit_position(alt_obs['transcript'], alt_spk['transcript'])
+                                trans_obs_sub = alt_obs['transcript'][pos_idx:pos_idx + len(alt_spk['transcript'])]
+                                transcript_split_value.append(max(similarity_list))
+                                transcript_split_word.append(trans_obs_sub)
+                                max_value = transcript_split_value.index(max(transcript_split_value))
+                                if similarity(trans_obs_sub, alt_spk['transcript']) > 2 / 3:
+                                        print('Speaker in observer >>>>',trans_obs_sub)
+                                        TDS, TDE = get_time_delay(alt_obs, alt_spk)
+                                        time_delay_start_list.append(TDS)
+                                        time_delay_end_list.append(TDE)
+                                       
+                                        minele_s = get_min_time_delay(time_delay_start_list)
+                                        minele_e = get_min_time_delay(time_delay_end_list)
+                                        
+                                        alt_obs['time_delay_start'] = minele_s
+                                        alt_obs['time_delay_end'] = minele_e
+        
+                                        alt_obs['transcript_split'] = alt_spk['transcript']
+                                        
+                                        alt_obs['transcript_list'].append(transcript_split_word[max_value])
+        
+                                        print('Observer [{} to {}] : {} '.format(alt_obs['words'][0]['startTime'], alt_obs['words'][-1]['endTime'], alt_obs['transcript']))
+                                        print('time_delay_start ', alt_obs['time_delay_start'])
+                                        print('time_delay_end ', alt_obs['time_delay_end'])
+        
+                                        # TDS_split, TDE_split = get_trans_time(alt_obs, alt_spk)
+                                        # print('Observer [{} to {}] : {} '.format(TDS_split, TDE_split, alt_obs['transcript']))
+                                        transcript_split_value = []
+                                        transcript_split_word = []  # reset word and value list
+
+
+    path_obs_new = open(path_obs.replace('.json', '_new.json'), 'w', encoding='utf-8')
+    json.dump(data_obs, path_obs_new, ensure_ascii=False)
+
+    print('wrote json to:', path_obs_new)
+
+if __name__ == '__main__':    
+    main()
 
